85948b823cd6f04361e1419049bb72c4f5d46c49
Aisa Mukukenova <mkaysa@gmail.com>
2019-06-11 18:06:30 +0100

    improved show page and create cocktails page

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,7 +2,7 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-fixed');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= 400) {
+      if (window.scrollY >= 200) {
         navbar.classList.add('navbar-fixed-white');
       } else {
         navbar.classList.remove('navbar-fixed-white');
